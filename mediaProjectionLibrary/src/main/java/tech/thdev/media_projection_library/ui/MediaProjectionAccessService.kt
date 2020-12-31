4fe26fa204bac66fd30e906da9b1204630e7b3d5
kimss <kimss@rsupport.com>
2020-12-31 16:57:57 +0900

    Revert "replace constants with real device size"
    This reverts commit a0e512a4bef71bb2cfe76b4d848f0016c1326db7.

--- mediaProjectionLibrary/src/main/java/tech/thdev/media_projection_library/ui/MediaProjectionAccessService.kt
+++ mediaProjectionLibrary/src/main/java/tech/thdev/media_projection_library/ui/MediaProjectionAccessService.kt
@@ -25,13 +25,14 @@ import tech.thdev.media_projection_library.constant.ACTION_SELF_STOP
 import tech.thdev.media_projection_library.constant.ACTION_START
 import tech.thdev.media_projection_library.constant.ACTION_STOP
 import tech.thdev.media_projection_library.constant.DEFAULT_VALUE_PROJECTION_NAME
+import tech.thdev.media_projection_library.constant.DEFAULT_VALUE_SIZE_HEIGHT
+import tech.thdev.media_projection_library.constant.DEFAULT_VALUE_SIZE_WIDTH
 import tech.thdev.media_projection_library.constant.EXTRA_PROJECTION_NAME
 import tech.thdev.media_projection_library.constant.EXTRA_REQUEST_DATA
 import tech.thdev.media_projection_library.constant.EXTRA_RESULT_CODE
 import tech.thdev.media_projection_library.constant.EXTRA_SIZE_HEIGHT
 import tech.thdev.media_projection_library.constant.EXTRA_SIZE_WIDTH
 import tech.thdev.media_projection_library.constant.EXTRA_SURFACE
-import tech.thdev.media_projection_library.util.DeviceSize
 
 open class MediaProjectionAccessService : Service() {
 
@@ -51,11 +52,11 @@ open class MediaProjectionAccessService : Service() {
             }
 
         fun newStartMediaProjection(
-                context: Context,
-                surface: Surface,
-                projectionName: String = DEFAULT_VALUE_PROJECTION_NAME,
-                width: Int = DeviceSize(context).size.width,
-                height: Int = DeviceSize(context).size.height
+            context: Context,
+            surface: Surface,
+            projectionName: String = DEFAULT_VALUE_PROJECTION_NAME,
+            width: Int = DEFAULT_VALUE_SIZE_WIDTH,
+            height: Int = DEFAULT_VALUE_SIZE_HEIGHT
         ): Intent =
             newService(context).apply {
                 putExtra(EXTRA_SURFACE, surface)
@@ -167,16 +168,16 @@ open class MediaProjectionAccessService : Service() {
         startMediaProjection(
             surface = intent.getParcelableExtra(EXTRA_SURFACE) as Surface,
             projectionName = intent.getStringExtra(EXTRA_PROJECTION_NAME) ?: DEFAULT_VALUE_PROJECTION_NAME,
-            width = intent.getIntExtra(EXTRA_SIZE_WIDTH, DeviceSize(this).size.width),
-            height = intent.getIntExtra(EXTRA_SIZE_HEIGHT, DeviceSize(this).size.height)
+            width = intent.getIntExtra(EXTRA_SIZE_WIDTH, DEFAULT_VALUE_SIZE_WIDTH),
+            height = intent.getIntExtra(EXTRA_SIZE_HEIGHT, DEFAULT_VALUE_SIZE_HEIGHT)
         )
     }
 
     fun startMediaProjection(
         surface: Surface,
         projectionName: String = DEFAULT_VALUE_PROJECTION_NAME,
-        width: Int = DeviceSize(this).size.width,
-        height: Int = DeviceSize(this).size.height
+        width: Int = DEFAULT_VALUE_SIZE_WIDTH,
+        height: Int = DEFAULT_VALUE_SIZE_HEIGHT
     ) {
         if (::mediaProjection.isInitialized) {
             virtualDisplay = mediaProjection.createVirtualDisplay(
